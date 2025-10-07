35a9a41d9ccc46296d42c3a783bfc4c4115b1dda
Andrei Solomon <andrei.f.solomon@gmail.com>
2025-10-06 21:50:22 -0400

    Initial Commit

---
+++ src/main/java/com/citi/rest/CitiSampleRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.citi.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiSampleRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiSampleRestServiceApplication.class, args);
+    }
+
+}
